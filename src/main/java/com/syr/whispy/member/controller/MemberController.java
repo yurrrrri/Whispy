927a2e18a1d8f490861d2785bbdaa777c1c1956a
yurison <yu_ri_son@naver.com>
2023-07-14 10:52:07 +0900

    feat: Member 관련 코드 구현
    Member 엔티티와 연관된 코드를 구현하고, 컨트롤러 단의 login 메서드 및 html의 뼈대를 작성했습니다.

---
+++ src/main/java/com/syr/whispy/member/controller/MemberController.java
@@ -0,0 +1,21 @@
+package com.syr.whispy.member.controller;
+
+import com.syr.whispy.member.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/usr")
+@RequiredArgsConstructor
+public class MemberController {
+
+    private MemberService memberService;
+
+    @GetMapping("/login")
+    public String showLogin() {
+        return "usr/login";
+    }
+
+}
