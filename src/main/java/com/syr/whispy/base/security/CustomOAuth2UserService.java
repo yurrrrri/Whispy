0265dbd0ee19e1e3adb97ff5dd274707f3add2fe
yurison <yu_ri_son@naver.com>
2023-07-14 09:52:04 +0900

    refactor: 어노테이션 삭제
    필요 없는 어노테이션을 삭제했습니다.

--- src/main/java/com/syr/whispy/base/security/CustomOAuth2UserService.java
+++ src/main/java/com/syr/whispy/base/security/CustomOAuth2UserService.java
@@ -20,7 +20,6 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
     private final MemberService memberService;
 
     @Override
-    @Transactional
     public OAuth2User loadUser(
             OAuth2UserRequest userRequest
     ) throws OAuth2AuthenticationException {
