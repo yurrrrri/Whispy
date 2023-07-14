c5ae08e2dbc057224648758bf58c4f7345339028
yurison <yu_ri_son@naver.com>
2023-07-14 09:31:12 +0900

    feat: Member 도입과 OAuth2, Security 적용
    Spring Security와 OAuth2 도입을 위해 Member 엔티티, 서비스 등을 구현하고 적용했습니다.

---
+++ src/main/java/com/syr/whispy/member/entity/Member.java
@@ -0,0 +1,49 @@
+package com.syr.whispy.member.entity;
+
+import com.syr.whispy.base.entity.BaseEntity;
+import com.syr.whispy.base.entity.Role;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.Date;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder(toBuilder = true)
+@Document(collection = "member")
+public class Member extends BaseEntity {
+
+    private String username;
+
+    private String password;
+
+    private String nickname;
+
+    private String email;
+
+    private Date birthday;
+
+    private String image;
+
+    private Set<Role> authorities = new HashSet<>();
+
+    public List<? extends GrantedAuthority> getAuthorities() {
+        return authorities.stream()
+                .map(i -> new SimpleGrantedAuthority("ROLE_" + i.name()))
+                .toList();
+    }
+
+    public void addRole(Role role) {
+        this.authorities.add(role);
+    }
+
+}
