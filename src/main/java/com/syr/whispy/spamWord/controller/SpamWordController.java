833841c39f93c2d34aaea155efbf3ae83d565c5f
yurison <yu_ri_son@naver.com>
2023-07-17 08:13:50 +0900

    feat: SpamWord 추가 구현
    SpamWord의 create 메서드와 html 뼈대를 작성했습니다.

--- src/main/java/com/syr/whispy/spamWord/controller/SpamWordController.java
+++ src/main/java/com/syr/whispy/spamWord/controller/SpamWordController.java
@@ -1,12 +1,17 @@
 package com.syr.whispy.spamWord.controller;
 
+import com.syr.whispy.spamWord.SpamWordCreateDto;
 import com.syr.whispy.spamWord.service.SpamWordService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@PreAuthorize("hasRole('ROME_ADMIN')")
 @RequiredArgsConstructor
 @Controller
 @RequestMapping("/spam")
@@ -18,6 +23,17 @@ public class SpamWordController {
     public String getAllSpamWordList(Model model) {
         model.addAttribute("spamWords", spamWordService.findAll());
         return "spam/list";
+    }
+
+    @GetMapping("/create")
+    public String createSpamWord() {
+        return "spam/create";
+    }
+
+    @PostMapping("/create")
+    public String createSpamWord(@Valid SpamWordCreateDto dto) {
+        spamWordService.create(dto);
+        return "redirect:/spam/list";
     }
 
 }
