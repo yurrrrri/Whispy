eb5b93b6715cb432c60b96bf2715010b9dd69359
yurison <yu_ri_son@naver.com>
2023-07-22 17:53:31 +0900

    refactor: 자잘한 수정
    필요 없는 부분을 삭제하거나 수정이 필요한 부분을 수정했습니다.

--- src/main/java/com/syr/whispy/base/security/CustomOAuth2UserService.java
+++ src/main/java/com/syr/whispy/base/security/CustomOAuth2UserService.java
@@ -9,7 +9,6 @@ import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserServ
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Collection;
 import java.util.Map;
--- src/main/java/com/syr/whispy/base/security/config/SecurityConfig.java
+++ src/main/java/com/syr/whispy/base/security/config/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.syr.whispy.base.security.config;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
@@ -8,7 +7,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
-@RequiredArgsConstructor
 @EnableMethodSecurity
 @EnableWebSecurity
 @Configuration
@@ -19,7 +17,7 @@ public class SecurityConfig {
         http
                 .formLogin(form -> form.loginPage("/usr/login").defaultSuccessUrl("/usr/timeline"))
                 .oauth2Login(oauth -> oauth.loginPage("/usr/login").defaultSuccessUrl("/usr/timeline"))
-                .logout(logout -> logout.logoutUrl("/usr/logout"));
+                .logout(logout -> logout.logoutUrl("/usr/logout").logoutSuccessUrl("/usr/login"));
         http
                 .authorizeHttpRequests(auth -> auth.requestMatchers("/**").permitAll());
 
