64a3ac34170d25d9fe87e9a45aaa7289a7edcc4f
yurison <yu_ri_son@naver.com>
2023-07-26 23:29:06 +0900

    feat: PostController 조회 메서드 구현
    Post의 목록 또는 특정 Post를 조회하는 메서드와 html의 뼈대를 작성했습니다.

--- src/main/java/com/syr/whispy/post/controller/PostController.java
+++ src/main/java/com/syr/whispy/post/controller/PostController.java
@@ -1,8 +1,16 @@
 package com.syr.whispy.post.controller;
 
+import com.syr.whispy.member.entity.Member;
+import com.syr.whispy.member.service.MemberService;
 import com.syr.whispy.post.service.PostService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequiredArgsConstructor
@@ -11,5 +19,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class PostController {
 
     private final PostService postService;
+    private final MemberService memberService;
+
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/list")
+    public String showMyEntirePostList(Model model, @AuthenticationPrincipal OAuth2User principal) {
+        Member member = memberService.findByUsernameAndGet(principal.getName());
+        model.addAttribute("posts", postService.findByWriter(member.getUsername()));
+        return "post/list";
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/{id}")
+    public String showPostDetail(@PathVariable String id, Model model) {
+        model.addAttribute("post", postService.findByIdAndGet(id));
+        return "post/detail";
+    }
 
 }
