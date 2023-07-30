245f3c6b68f713d212691c78c5112c8f11eea120
yurison <yu_ri_son@naver.com>
2023-07-30 20:50:41 +0900

    feat: PostController CRUD 구현
    Post에 대한 CRUD 메서드를 구현했습니다.

--- src/main/java/com/syr/whispy/post/controller/PostController.java
+++ src/main/java/com/syr/whispy/post/controller/PostController.java
@@ -2,7 +2,10 @@ package com.syr.whispy.post.controller;
 
 import com.syr.whispy.member.entity.Member;
 import com.syr.whispy.member.service.MemberService;
+import com.syr.whispy.post.dto.PostCreateDto;
+import com.syr.whispy.post.dto.PostUpdateDto;
 import com.syr.whispy.post.service.PostService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -11,8 +14,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@PreAuthorize("isAuthenticated()")
 @RequiredArgsConstructor
 @Controller
 @RequestMapping("/post")
@@ -21,7 +26,6 @@ public class PostController {
     private final PostService postService;
     private final MemberService memberService;
 
-    @PreAuthorize("isAuthenticated()")
     @GetMapping("/list")
     public String showMyEntirePostList(Model model, @AuthenticationPrincipal OAuth2User principal) {
         Member member = memberService.findByUsernameAndGet(principal.getName());
@@ -29,11 +33,46 @@ public class PostController {
         return "post/list";
     }
 
-    @PreAuthorize("isAuthenticated()")
     @GetMapping("/{id}")
     public String showPostDetail(@PathVariable String id, Model model) {
         model.addAttribute("post", postService.findByIdAndGet(id));
         return "post/detail";
+    }
+
+    @GetMapping("/create")
+    public String createPost() {
+        return "post/create";
+    }
+
+    @PostMapping("/create")
+    public String createPost(@Valid PostCreateDto dto) {
+        postService.create(dto);
+        return "redirect:/post/list";
+    }
+
+    @GetMapping("/update/{id}")
+    public String updatePost(@PathVariable String id, Model model) {
+        model.addAttribute("post", postService.findByIdAndGet(id));
+        return "post/update";
+    }
+
+    @PostMapping("/update/{id}")
+    public String updatePost(@PathVariable String id, @Valid PostUpdateDto dto) {
+        postService.update(dto);
+        return "redirect:/post/%s".formatted(id);
+    }
+
+    @GetMapping("/delete/{id}")
+    public String softDeletePost(@PathVariable String id) {
+        postService.softDelete(id);
+        return "redirect:/post/list";
+    }
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @GetMapping("/hardDelete/{id}")
+    public String hardDeletePost(@PathVariable String id) {
+        postService.hardDelete(id);
+        return "redirect:/post/list";
     }
 
 }
